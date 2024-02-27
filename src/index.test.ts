4e817f32d04d3fa6eb16c96b23d2b03633d0880f
Mikita Taukachou <edloidas@gmail.com>
2024-02-27 12:18:04 +0100

    Update default quotes

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,9 @@
-import assert from "assert/strict";
-import test, { describe } from "node:test";
-import { add } from "./index";
+import assert from 'assert/strict';
+import test, { describe } from 'node:test';
+import { add } from './index';
 
-describe("add()", () => {
-  test("add two numbers", () => {
+describe('add()', () => {
+  test('add two numbers', () => {
     assert.strictEqual(add(1, 2), 3);
   });
 });
